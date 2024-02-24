d01bb50765d47d57356b5575ad4e902bf5f8e8bd
Yorshik <kirkidzapp@gmail.com>
2024-02-24 03:14:07 +0300

    Поиск аптека 2.0

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import math
 import sys
 from io import BytesIO
 
 import requests
 from PIL import Image
 
-from get_spn import get_spn
-
-toponym_to_find = " ".join(sys.argv[1:])
+apikey = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+map_api_server = "http://static-maps.yandex.ru/1.x/"
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+search_api_server = 'https://search-maps.yandex.ru/v1/'
+
+# geocoder
+toponym_to_find = " ".join(sys.argv[1:])
 geocoder_params = {
     "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
     "geocode": toponym_to_find,
@@ -19,15 +23,60 @@ if not response:
 json_response = response.json()
 toponym = json_response["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+toponym_coordinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coordinates.split(" ")
+
+# search
+address_ll = f"{toponym_longitude},{toponym_lattitude}"
+
+search_params = {
+    "apikey": apikey,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+
+response = requests.get(search_api_server, params=search_params)
+if not response:
+    print(response.status_code, response.reason)
+    quit()
+js = response.json()
+
+organization = js['features'][0]
+org_longitude, org_lattitude = organization['geometry']['coordinates']
+s = round(
+    math.sqrt(
+        math.pow(
+            111 *
+            math.cos((toponym_longitude + org_longitude) / 2) *
+            abs(float(toponym_longitude) - org_longitude),
+            2
+        )
+        +
+        math.pow(
+            111 *
+            abs(float(toponym_lattitude) - org_lattitude),
+            2
+        )
+    )
+    , 2
+)
+point = f"{','.join(map(str, organization['geometry']['coordinates']))},org"
+out = f'''
+Адрес - {organization['properties']['description']}
+Название - {organization['properties']['name']}
+Часы работы - {organization['properties']['CompanyMetaData']['Hours']['text']}
+Расстояние от исходной точки - ~{s}км
+'''
+print(out)
+
+# map
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": get_spn(toponym),
     "l": "map",
-    "pt": f'{toponym_longitude},{toponym_lattitude},comma'
+    "pt": f'{toponym_longitude},{toponym_lattitude},comma~{point}'
 }
-map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
 Image.open(
     BytesIO(
